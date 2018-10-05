51efc82e28135105ab1f0c3c5f52d0cc82aacb5f
Calle <36276161+qalle-fivem@users.noreply.github.com>
2018-10-05 15:48:47 +0200

    fixed

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,18 @@
-local PlayerData                = {}
+PlayerData                = {}
 
 ESX                             = nil
 
 Citizen.CreateThread(function()
-  while ESX == nil do
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-    Citizen.Wait(1)
-  end
-  PlayerData = ESX.GetPlayerData()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(10)
+	end
+
+	while ESX.GetPlayerData() == nil do
+		Citizen.Wait(10)
+	end
+
+  	PlayerData = ESX.GetPlayerData()
 end)
 
 RegisterCommand("job", function(source, args, raw)
